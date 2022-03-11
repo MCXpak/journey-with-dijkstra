b3c62c2e5540156d93be53cf494a478afc598e34
Mourad Lasga Terceras <mouradlasga@gmail.com>
2022-03-11 23:52:24 +0000

    changed to two js, produced wobles for nodes

---
+++ script.js
@@ -0,0 +1,45 @@
+let main = document.getElementById('main'), two = new Two({
+    type: Two.Types.svg,
+    fullscreen: true,
+    autostart: true
+  }).appendTo(main);
+
+let circ = two.makeCircle(two.width/2, two.height/2, 50 ,50);
+
+const gravity = new Two.Vector(0, 0.66);
+
+circ.velocity = new Two.Vector();
+
+let circles = []
+let circle_vels = []
+
+circles.push(circ)
+circle_vels.push([0,0])
+console.log(circle_vels)
+let xPos = 0;
+let yPos = 0;
+
+window.addEventListener('pointerdown', pointerdown, false);
+
+function pointerdown(e){
+    let x = event.clientX;
+    let y = event.clientY;
+    let n = two.makeCircle(x, y, 50 ,50);
+    n.velocity = new Two.Vector();
+    circles.push(n);
+    circle_vels.push([0,0])
+}
+
+function wobble(shape, node, l){
+    shape.translation.addSelf(shape.velocity);
+    shape.velocity.x = (Math.sin(l[node][0])/10)
+    shape.velocity.y = (Math.sin(l[node][1])/10)
+    l[node][0] += Math.random() * 0.05;
+    l[node][1] += Math.random() * 0.05;
+}
+
+two.bind('update', function() {
+    for(let node = 0; node < circles.length; node++){
+        wobble(circles[node], node, circle_vels);
+    }
+});
