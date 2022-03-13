dccc6d504016bff9c262c78168b5c40ae8fd307a
Mourad Lasga Terceras <mouradlasga@gmail.com>
2022-03-13 23:27:09 +0000

    can now produce vertices between nodes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let nodes = [];
 let node_vels = [];
+let vertices = [];
+let verticesToAdd = [];
 let tx = 0;
 let ty = 0;
 
@@ -24,6 +26,19 @@ class Node{
     }
 }
 
+class Vertex{
+    constructor(startX, startY, endX, endY){
+        this.position = [startX, startY, endX, endY];
+        this.vertex = line(this.position[0], this.position[1], this.position[2], this.position[3]);
+    }
+    followNodes(){
+        
+    }
+    show(){
+        this.vertex = line(this.position[0], this.position[1], this.position[2], this.position[3]);
+    }
+}
+
 function setup() {
     createCanvas(720, 400);
     background(127);
@@ -32,6 +47,10 @@ function setup() {
 function draw() {
     background(127);
     line(100,100,200,200);
+    for (let i = 0; i < vertices.length; i++) {
+        vertices[i].show();
+    }
+
     for (let i = 0; i < nodes.length; i++) {
         nodes[i].show();
         nodes[i].wobble(i);
@@ -45,11 +64,22 @@ function clickedOnNode(){
         console.log()
         let d = dist(mouseX, mouseY, currentNode.position.x, currentNode.position.y);
         if (d < 25) {
-            let newNode = new Node(currentNode.position.x, currentNode.position.y);
-            newNode.fill = [143,141,29];
-            nodes[node] = newNode;
-            console.log(currentNode);
-            console.log("in node")
+            //let newNode = new Node(currentNode.position.x, currentNode.position.y);
+            //newNode.fill = [143,141,29];
+            if (verticesToAdd.length <= 4){
+                verticesToAdd.push(currentNode.position.x);
+                verticesToAdd.push(currentNode.position.y);
+                console.log("Vertices added:", verticesToAdd);
+            }
+            if (verticesToAdd.length == 4){
+                console.log("2");
+                let vertex = new Vertex(verticesToAdd[0],verticesToAdd[1],verticesToAdd[2],verticesToAdd[3]);
+                vertices.push(vertex);
+                while (verticesToAdd.length) { verticesToAdd.pop(); }
+            }
+            //nodes[node] = newNode;
+            //console.log(currentNode);
+            //console.log("in node")
             return true
         }
         
