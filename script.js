5c70fab20dead142cc7d6f31b9c4bd2d4c71377e
Mourad Lasga Terceras <mouradlasga@gmail.com>
2022-03-13 22:59:37 +0000

    completely changed everything, changed to p5.js

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-let main = document.getElementById('main'), two = new Two({
-    type: Two.Types.svg,
-    fullscreen: true,
-    autostart: true
-  }).appendTo(main);
 
-let circ = two.makeCircle(two.width/2, two.height/2, 50 ,50);
+let nodes = [];
+let node_vels = [];
+let tx = 0;
+let ty = 0;
 
-const gravity = new Two.Vector(0, 0.66);
-
-circ.velocity = new Two.Vector();
-
-let circles = []
-let circle_vels = []
-
-circles.push(circ)
-circle_vels.push([0,0])
-console.log(circle_vels)
-let xPos = 0;
-let yPos = 0;
-
-function randColor(){
-    return Math.floor(Math.random()*16777215).toString(16);
-}
-
-randColor();
-
-window.addEventListener('pointerdown', pointerdown, false);
-
-function createNode(){
-    let x = event.clientX;
-    let y = event.clientY;
-    let n = two.makeCircle(x, y, 50 ,50);
-    n.fill = "#" + randColor();
-    n.velocity = new Two.Vector();
-    circles.push(n);
-    circle_vels.push([0,0])
-}
-
-function pointerdown(e){
-    createNode()
-}
-
-function wobble(shape, node, l){
-    shape.translation.addSelf(shape.velocity);
-    shape.velocity.x = (Math.sin(l[node][0])/10)
-    shape.velocity.y = (Math.sin(l[node][1])/10)
-    l[node][0] += Math.random() * 0.05;
-    l[node][1] += Math.random() * 0.05;
-}
-
-two.bind('update', function() {
-    for(let node = 0; node < circles.length; node++){
-        wobble(circles[node], node, circle_vels);
+class Node{
+    constructor(xPos, yPos){
+        this.node = ellipse(xPos, yPos, 50, 50);
+        this.position = createVector(xPos, yPos);
+        this.velocity = createVector(0, 0);
+        this.fill = [200,200,200];
     }
-});
+    wobble(i){
+        let newX = node_vels[i][0];
+        let newY = node_vels[i][1];
+        this.position.add(Math.sin(newX)/10,Math.sin(newY)/10);
+        node_vels[i][0] += Math.random() * 0.05;
+        node_vels[i][1] += Math.random() * 0.06;
+    }
+    show(){
+        fill(this.fill[0],this.fill[1],this.fill[2]);
+        this.node = ellipse(this.position.x, this.position.y, 50, 50);
+    }
+}
+
+function setup() {
+    createCanvas(720, 400);
+    background(127);
+}
+
+function draw() {
+    background(127);
+    line(100,100,200,200);
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].show();
+        nodes[i].wobble(i);
+    }
+}
+
+function clickedOnNode(){
+    for (let node = 0; node < nodes.length; node++) {
+        let currentNode = nodes[node];
+        console.log("yes")
+        console.log()
+        let d = dist(mouseX, mouseY, currentNode.position.x, currentNode.position.y);
+        if (d < 25) {
+            let newNode = new Node(currentNode.position.x, currentNode.position.y);
+            newNode.fill = [143,141,29];
+            nodes[node] = newNode;
+            console.log(currentNode);
+            console.log("in node")
+            return true
+        }
+        
+    }
+}
+// When the user clicks the mouse
+function mousePressed() {
+    if(!clickedOnNode()){
+        let newNode = new Node(mouseX, mouseY);
+        nodes.push(newNode);
+        node_vels.push([0,0]);
+        console.log(nodes);
+    }    
+}
